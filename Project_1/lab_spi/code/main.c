20f71b3b34aaaff79300bbeff5b79b2d9a03170f
Mysterium-sch <lamblily90@gmail.com>
2023-09-11 12:30:42 -0400

    started part 2 - lab 1

--- Project_1/lab_spi/code/main.c
+++ Project_1/lab_spi/code/main.c
@@ -32,14 +32,16 @@ int main(int argc, char** argv) {
 		//printf("Nsignals = %d, nsamples = %d\n", w->nsignals, w->nsamples);
 	}
 	int cur = 3;
-	int mosi[16];
-	int miso[16];
+	int mosi[w->nsamples];
+	int miso[w->nsamples];
 	bool read = false;
 	int reads = -1;
 	unsigned int CPOL = signal_at_idx(w,4,0);
 	unsigned int clk = CPOL;
 	bool first = false;
 	unsigned int CPHA = signal_at_idx(w,5,0);
+	int inter = -1;
+	bool streaming = false;
 
 	if(CPHA == 0) {
 		first = true;
@@ -55,6 +57,7 @@ int main(int argc, char** argv) {
 				if(first || (CPOL==CPHA && (clk==1 && signal_at_idx(w,0,cur+1)==0)) || (CPOL!=CPHA && (clk==0 && signal_at_idx(w,0,cur+1)==1))) {
 					read = true;
 					reads = reads + 1;
+					inter = inter + 1;
 					first = false;
 				} else {
 					read = false;
@@ -69,18 +72,35 @@ int main(int argc, char** argv) {
 		}
 		cur = cur + 1; 
 
-		if(reads==15) {
+		if(reads==15 || (streaming && inter/7==1)) {
 		                int R_W = mosi[reads-9]; 
-                                //int stream = mosi[reads-8];
-                                int address = mosi[reads-10] + 2*mosi[reads-11] + 4*mosi[reads-12] + 8*mosi[reads-13] + 16*mosi[reads-14] + 32*mosi[reads-15]; 
-                                int value_wr = mosi[reads] + 2*mosi[reads-1] + 4*mosi[reads-2] + 8*mosi[reads-3] + 16*mosi[reads-4] + 32*mosi[reads-5] + 64*mosi[reads-6] + 128*mosi[reads-7];
-                                int value_rd = miso[reads] + 2*miso[reads-1] + 4*miso[reads-2] + 8*miso[reads-3] + 16*miso[reads-4] + 32*miso[reads-5] + 64*miso[reads-6] + 128*miso[reads-7];
-                               if(R_W == 0) {
-					printf("RD %02x %02x\n", address, value_rd);
-				} else {
-					printf("WR %02x %02x\n", address, value_wr);
+                                int stream = mosi[reads-8];
+				if(stream == 1) {
+					streaming = !streaming;
 				}
-			       reads = -1;
+                                int address = mosi[reads-10] + 2*mosi[reads-11] + 4*mosi[reads-12] + 8*mosi[reads-13] + 16*mosi[reads-14] + 32*mosi[reads-15]; 
+                                int value_wr[w->nsamples];
+				value_wr[((reads/2)/inter)==1] = mosi[reads] + 2*mosi[reads-1] + 4*mosi[reads-2] + 8*mosi[reads-3] + 16*mosi[reads-4] + 32*mosi[reads-5] + 64*mosi[reads-6] + 128*mosi[reads-7];
+                                int value_rd[w->nsamples];
+				value_rd[((reads/2)/inter)==1] = miso[reads] + 2*miso[reads-1] + 4*miso[reads-2] + 8*miso[reads-3] + 16*miso[reads-4] + 32*miso[reads-5] + 64*miso[reads-6] + 128*miso[reads-7];
+
+				if(R_W == 0) {
+					printf("RD %02x", address);
+                                	for(int w=0; w<=((reads/2)/inter); w++) {
+                                        	printf(" %02x", value_rd[w]);
+                                	}
+                                printf("\n");
+				} else {
+					printf("WR %02x", address);
+                                	for(int z=0; z<=((reads/2)/inter);z++) {
+                                		printf(" %02x", value_wr[z]);
+                                	}
+                                	printf("\n");
+				}
+			       if(stream == 0) {
+			       	reads = -1;
+				}
+			       inter = -1;
 		}
 		
 	}
